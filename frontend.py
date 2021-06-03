d395cb14408790bde36f44b004fee8cd94821f69
Michael Darmawan <darmawan.mori@gmail.com>
2021-06-03 16:27:43 -0500

    initial commit

---
+++ frontend.py
@@ -0,0 +1,160 @@
+from typing import Generator
+from blessed import Terminal
+import sys
+import os
+import json
+# import pathlib
+from pathlib import Path
+import recipe
+
+EXIT_COMMAND="exit"
+CONFIG_REL_PATH="rcpconfig.txt"
+term = Terminal()
+
+def cwd_path():
+    return Path(os.getcwd())
+
+def init_settings():
+    config_path = cwd_path()/CONFIG_REL_PATH
+    if config_path.exists():
+        print( f"Found a settings file at {str(config_path)}!" )
+
+def splitter(line:str):
+    #probably deprecateable.
+    return list(tokenizer(line))
+
+def tokenizer(line:str):
+    """Generator, returns true upon ending"""
+    WHITESPACE=0
+    TOKEN=1
+    DQUOTE=2
+    SQUOTE=3
+    state = WHITESPACE
+    reg0 = 0
+    for i in range(len(line)):
+        char = line[i]
+        if state == WHITESPACE:
+            if char == "\"":
+                state = DQUOTE
+                reg0 = i+1
+            elif char == "'":
+                state = SQUOTE
+                reg0 = i+1
+            elif not char.isspace():
+                state = TOKEN
+                reg0 = i
+        elif state == TOKEN:
+            if char.isspace():
+                state = WHITESPACE
+                yield line[reg0:i+1]
+        elif state == SQUOTE:
+            if char == "'":
+                state = WHITESPACE
+                yield line[reg0:i]
+        elif state == DQUOTE:
+            if char == "\"":
+                state = WHITESPACE
+                yield line[reg0:i]
+    yield line[reg0:]
+    return True
+
+def script_mode(script:Path):
+    with script.open("r") as infile:
+        for line in infile:
+            interpret_command(line)
+
+def console_mode():
+    def prompt():
+        return f"{term.green}{os.getcwd()} {term.yellow}${term.normal} "
+    
+    goon = True
+    imp = input(prompt())
+    #default mode is file-exploring mode
+    while goon:
+        if imp.startswith(EXIT_COMMAND):
+            goon=False
+        else: 
+            interpret_command(imp)
+            imp = input(prompt())
+
+def interpret_command(cmd:str):
+    tokens = tokenizer(cmd)
+    root_cmd = next(tokens)
+    if(root_cmd == "cd"):
+        os.chdir(Path(next(tokens)))
+    elif(root_cmd == "ls"):
+        #TODO: dumb ls, only does current dir
+        # print(os.getcwd())
+        for child in cwd_path().iterdir():
+            child_type = "D" if child.is_dir() else "F"
+            print(f"  {child_type} - {child.name}")
+    elif(root_cmd == "pwd"):
+        print(os.getcwd())
+    elif(root_cmd == "echo"):
+        pass
+    elif(root_cmd == "help"):
+        #TODO: print all commands
+        pass
+    elif(root_cmd == "open"):
+        rcp_path = cwd_path()/next(tokens)
+        #TODO:not sure if i need more things
+        recipe_mode(rcp_path, tokens)
+    else: 
+        print(f"{term.red}Command not recognized. enter '{term.normal}help{term.red}' to see available commands")
+
+def recipe_mode(rcp_path:Path, args_gen):
+    #not sure if args is needed, will be agenerator
+    with rcp_path.open("r") as rcpfile:
+        prompt=f"Current Recipe:{term.blue}{rcp_path.name} {term.yellow}$${term.normal}"
+        imp = input(prompt)
+        goon = True
+        while goon:
+            goon = manip_recipe(imp)
+            if goon:
+                imp = input(prompt)
+
+def manip_recipe(cmd:str):
+    #TODO: implement with reflection?
+    #no exec()... let's copy kubectl-style commands
+    #format is: 
+    tokens = tokenzier(cmd)
+    root = next(tokenizer)
+    if root == "close":
+        return False
+    
+    return True
+
+# def display_file(filepath):
+#     term=Terminal()
+#     with term.fullscreen(), \
+#         open(filepath, 'r') as file:
+#         for line in file:
+#             print(line)
+        
+def main():
+    """
+    env vars (home, mode/state) (local settings file?)
+    file navigation mode (cd ls pwd)
+    help
+    recipe editing mode
+        create recipe
+        edit steps
+        scale recipe
+        optional feature: tab autocomplete?
+        stretch feature: treeNode-based recipes
+    """
+    init_settings()
+    if len(sys.argv) > 1:
+        script_path = Path(sys.argv[1])
+        if script_path.exists():
+            script_mode(script_path)
+        else: 
+            pass
+    else:
+        console_mode()
+
+if __name__ == '__main__':
+    main()
+    print()
+    # tokens = tokenizer("abc def ghi")
+    # next(tokens)
---
+++ recipe.py
@@ -0,0 +1,192 @@
+import json
+
+class Recipe:
+    TITLE_KEY="title"
+    INGR_KEY="ingredients"
+    STEP_KEY="steps"
+    META_KEY="metadata"
+    
+    title:str
+    #K:V = str:IngredientAmount
+    ingredients:dict
+    steps:list
+
+    #TODO: stores other information, such as #served, author and whatever else.
+    metadata:dict
+    META_AUTHOR="author"
+    META_SERVES="serves"
+    META_SRCURL="src_url"
+    META_REF="refs"
+
+    def __init__(self, filepath=None):
+        self.title = ""
+        self.ingredients={}
+        self.steps=[]
+        if filepath is not None:
+            self.read_json
+        
+    def __str__(self):
+        #let's use Markdown
+        string_builder = []
+        string_builder.append(f"# {self.title}")
+        #TODO: metadata?
+
+        string_builder.append(f"\n## Ingredients")
+        for ingr, amt in self.ingredients.items():
+            string_builder.append(f"  - {amt.amount} {amt.unit} {ingr}")
+        
+        string_builder.append(f"\n## Instructions")
+        for step_num,  step in enumerate(self.steps):
+            string_builder.append(f"  {step_num+1}. step")
+        return "\n".join(string_builder)
+    
+
+    def write_json(self, filepath):
+        with open(filepath,"w",) as outfile:
+            self_dict = {}
+            self_dict[self.TITLE_KEY] = self.title
+            self_dict[self.INGR_KEY] = {ingr:amt.get_tuple() for ingr, amt in self.ingredients.items()}
+            self_dict[self.STEP_KEY] = self.steps
+            self_dict[self.META_KEY] = self.metadata
+            json.dump(self_dict, outfile,indent=4)
+        
+
+    def read_json(self, filepath):
+        """
+            Could fail if format is wrong. Handle exceptions elsewhere?
+        """
+        with open(filepath,"r",) as infile:
+            my_dict=json.load(infile)
+            self.title = my_dict[self.TITLE_KEY]
+            self.steps = my_dict[self.STEP_KEY]
+            self.ingredients = {ingr:IngredientAmount(*amt) for ingr, amt in my_dict[self.INGR_KEY]}
+            self.metadata = my_dict[self.META_KEY]
+
+    def scale_ingredients(self, factor:int):
+        """
+        Modify the ingredient amounts by a scale factor
+        Can be called by other methods.
+        """
+        for ingred in self.ingredients.values():
+            ingred.scale(factor)
+
+
+class IngredientAmount:
+    """
+    Carries the amount and unit, handles conversions.
+    
+    Could maybe be a tuple, but has methods for conversions.
+    """
+    amount:float
+    unit:str
+
+    def __init__(self, amount:float, unit:str):
+        pass
+    
+    def __str__(self):
+        return f"{self.amount:.2} {self.unit}"
+    
+    #repr is for debugging, shows more decimals
+    def __repr__(self):
+        return f"{self.amount} {self.unit}"
+    
+    def get_tuple(self):
+        return (self.amount, self.unit)
+
+    #conversion units, possible float imprecision, but good enough.
+    def scale(self, factor):
+        self.amount *= factor
+
+    #keys are lowercase, and some are abbreviated
+    VOLUME_CONV:dict = {
+        "ml": 1,
+        "liter":1000,
+        "cup":236.5875,
+        "tbsp":14.7868,
+        "tsp":4.92892,
+        "fl oz": 29.5735
+    }
+
+    MASS_CONV:dict = {
+        "grams":1 , 
+        "kg":1000,
+        "lbs": 453.592,
+        "oz":28.3495
+    }
+
+    def is_volume_unit(self, unit:str = None):
+        unit = self.unit if unit is None else unit
+        return unit in self.VOLUME_CONV
+
+    def is_mass_unit(self, unit:str = None):
+        unit = self.unit if unit is None else unit
+        return unit in self.MASS_CONV
+
+    def convert_volume(self, dest_unit:str, src_unit:str, amt:float):
+        """
+        Converts volume units, returns a float
+        Does not check whether units exist, which would throw a KeyError
+        """
+        factor = self.VOLUME_CONV[src_unit]/self.VOLUME_CONV[dest_unit]
+        return amt*factor
+
+    def convert_mass(self, dest_unit:str, src_unit:str, amt:float):
+        """
+        Converts mass units, returns a float
+        Does not check whether units exist, which would throw a KeyError
+        """
+        factor = self.MASS_CONV[src_unit]/self.MASS_CONV[dest_unit]
+        return amt*factor
+
+    def convert_density(self,amt:float, density:float=1, to_volume=True):
+        """
+        density is in g/ml
+        returns a float to convert between grams and milliliters
+        """
+        if to_volume:
+            return amt/density
+        else:
+            return amt*density
+
+    def convert_amount(self, dest_unit:str, src_unit:str=None, amt:float=None, density=1):
+        """
+        returns a float
+        """
+        src_unit = self.unit if src_unit is None else src_unit
+        amt = self.amount if amt is None else amt
+
+        src_is_vol = self.is_volume_unit(src_unit)
+        dest_is_vol = self.is_volume_unit(dest_unit)
+
+        if src_is_vol != dest_is_vol:
+            #needs density stuff
+            if src_is_vol:
+                src_ml = self.convert_volume("ml", src_unit, amt)
+                dest_g = self.convert_density(src_ml, density, dest_is_vol)
+                return self.convert_mass(dest_unit, "g", dest_g)
+            else:
+                src_g = self.convert_mass("g", src_unit, amt)
+                dest_ml = self.convert_density(src_g, density, dest_is_vol)
+                return self.convert_volume(dest_unit, "ml", dest_ml)
+        elif src_is_vol:
+            return self.convert_volume(dest_unit, src_unit, amt)
+        else:
+            return self.convert_mass(dest_unit, src_unit, amt)
+
+# class RecipeNode:
+#     next_node:RecipeNode
+#     name:str
+#     description:str
+#     #numeric, int or float, for sorting a flowchart's nodes
+#     step_number
+#     def __init__(self, step):
+#         self.step_number = step
+#     #comparison functions for sorting. 
+#     def __lt__(self, other:RecipeNode):
+#         return self.step_number < other.step_number
+#     def __eq__(self, other:RecipeNode):
+#         return self.step_number == other.step_number
+#     #
+
+
+
