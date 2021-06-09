5b570da733f07cb44a05a8922568fa2c23cfb302
Michael Darmawan <darmawan.mori@gmail.com>
2021-06-09 08:23:34 -0500

    fixes

--- ansible/testscript.sh
+++ ansible/testscript.sh
@@ -1,11 +1,12 @@
 # no shebang for all posix shells
 ./venv/bin/python3 app.py -b rcpscript.txt
 
+echo "Tests" > tests.log.txt
 for i in 1 2 3; do
   if cmp -s "test$i.json" "tests/answer$i.json"; then
-    echo "Passed test $i!" >> tests/log.txt
+    echo " Passed test $i!" >> tests/log.txt
   else
-    echo "Failed test $i!" >> tests/log.txt
+    echo " Failed test $i!" >> tests/log.txt
     echo "Failed a test! Check logs!"
   fi
 done
