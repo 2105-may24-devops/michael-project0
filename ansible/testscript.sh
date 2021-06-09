9a8670aaa95a796e2cb76559f2c14f45e2b7a3b7
Michael Darmawan <darmawan.mori@gmail.com>
2021-06-08 21:57:17 -0500

    presentation ready. for reals

--- ansible/testscript.sh
+++ ansible/testscript.sh
@@ -1,7 +1,8 @@
 # no shebang for all posix shells
-/venv/bin/python3 app.py rcpscript.txt
+./venv/bin/python3 app.py -b rcpscript.txt
 
 for i in 1 2 3; do
+  echo hi $i
   if cmp -s "test$i.json" "tests/answer$i.json"; then
     echo "Passed test $i!" > tests/log.txt
   else
